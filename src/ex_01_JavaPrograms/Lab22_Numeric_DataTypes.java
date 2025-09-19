d7516c0d1f91b9740d5ce8cda5e334ca161f7b7f
Snehal Prajapati <savitrideviprajapati@gmail.com>
2025-09-19 14:48:29 +0530

    java numeric data types

---
+++ src/ex_01_JavaPrograms/Lab22_Numeric_DataTypes.java
@@ -0,0 +1,14 @@
+package ex_01_JavaPrograms;
+
+public class Lab22_Numeric_DataTypes {
+    public static void main(String[] args) {
+        byte b  =10;
+        short s = 20;
+        int i = 2;
+        float f = 3.14f;
+        double d = 3.00000;
+        long l = 4567890L;
+        char c ='a';
+
+    }
+}
