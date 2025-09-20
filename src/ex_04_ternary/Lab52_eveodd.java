0b73f443f4b4167809ae0952a3fa12cb418f296f
Snehal Prajapati <savitrideviprajapati@gmail.com>
2025-09-21 01:27:56 +0530

    code_Ternary_Even/odd

---
+++ src/ex_04_ternary/Lab52_eveodd.java
@@ -0,0 +1,9 @@
+package ex_04_ternary;
+
+public class Lab52_eveodd {
+    public static void main(String[] args) {
+        int a = 10;
+        String result = a%2==0?"even":"odd";
+        System.out.println(result);
+    }
+}
